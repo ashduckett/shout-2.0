7e20d9553af899b67d4fd92035b01882ae4a1d44
Ash <ash.duckett@outlook.com>
2017-03-13 17:23:31 +0000

    A

--- github.php
+++ github.php
@@ -1,17 +1,8 @@
 <?php
-    shell_exec('su ');
-    
-    $output = null;
-    
-    $output = shell_exec('git pull 2>&1');
-    
-    // Returns ashduckett. ashduckett is allowed to do this stuff.
-    $user = shell_exec('whoami 2>&1');
-    
-    $dir = shell_exec('$PWD 2>&1');
-    error_log($dir, 3, 'errr.log');
-    
-    error_log(print_r($user, true), 3, 'errr.log');
-    
-    error_log(print_r($output, true), 3, 'errr.log');
-    error_log('push made', 3, 'errr.log');
+
+
+$old_path = getcwd();
+chdir('/home/ashduckett/public_html/shout-2.0');
+$output = shell_exec('./github.sh var1 var2');
+chdir($old_path);
+echo "<pre>$output</pre>";
---
+++ github.sh
@@ -0,0 +1,3 @@
+cd /home/ashduckett/public_html/shout-2.0 
+git pull;
+
