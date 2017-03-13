7b3487b950d2fd5215cf56184ab0984fa6f7a68e
Ash <ash.duckett@outlook.com>
2017-03-13 02:06:01 +0000

    Show output

--- github.php
+++ github.php
@@ -1,4 +1,6 @@
 <?php
-    $output = `git pull`;
+    $output = null;
+    
+    exec('git pull', $output);
     error_log(print_r($output, true), 3, 'errr.log');
     error_log('push made', 3, 'errr.log');
