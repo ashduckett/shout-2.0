743f20d8f106541f4e9e72dfd216198fd06c4cfd
Ash <ash.duckett@outlook.com>
2017-03-13 16:51:17 +0000

    whoami

--- github.php
+++ github.php
@@ -1,6 +1,13 @@
 <?php
+    shell_exec('su ');
+    
     $output = null;
     
     $output = shell_exec('git pull 2>&1');
+    $user = shell_exec('whoami 2>&1');
+    
+    
+    error_log(print_r($user, true), 3, 'errr.log');
+    
     error_log(print_r($output, true), 3, 'errr.log');
     error_log('push made', 3, 'errr.log');
