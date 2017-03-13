ca40282540e81725445d4d9e87925c29af6ba566
Ash <ash.duckett@outlook.com>
2017-03-13 17:33:32 +0000

    A

--- github.php
+++ github.php
@@ -5,7 +5,7 @@ $old_path = getcwd();
 echo '2';
 chdir('/home/ashduckett/public_html/shout-2.0');
 echo '3';
-$output = shell_exec('github.sh var1 var2');
+$output = shell_exec('github.sh 2>&1');
 echo '4';
 chdir($old_path);
 echo '5';
